533b44e64c8e6d6e4860edc9eb6695ed08519213
wangkun <kunwang1@jieyuechina.com>
2019-05-05 16:05:38 +0800

    init

--- upgrade/app.js
+++ upgrade/app.js
@@ -1,21 +1,13 @@
+const puppeteer = require('puppeteer');
 const express = require('express')
 const fs = require("fs");
 
 const app = express();
-let browser;
-let page;
-(async () => {
-    const puppeteer = require('puppeteer');
-    const devices = require('puppeteer/DeviceDescriptors');
-    const iPhone = devices['iPhone 6'];
-    browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: 'C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'
-    });
-    page = await browser.newPage();
-    // await page.emulate(iPhone);
-})()
+
+const MAX_WSE = 4;  //启动几个浏览器
+let WSE_LIST = []; //存储browserWSEndpoint列表
+init();
+
 
 app.get('/getHtml', function (req, res) {
     var url = req.query.url;
@@ -23,6 +15,12 @@ app.get('/getHtml', function (req, res) {
     console.log("url", url)
     var vocabulary = req.params.vocabulary;
     (async () => {
+        let tmp = Math.floor(Math.random() * MAX_WSE);
+
+        let browserWSEndpoint = WSE_LIST[tmp];
+        const browser = await puppeteer.connect({browserWSEndpoint});
+        const page = await browser.newPage();
+
         var resp = await page.goto(url, {timeout: 120000});
         var rawHtml = await resp.text();
         var re = new RegExp("<script.*?/script>|<link.*?>", "g");
@@ -32,14 +30,39 @@ app.get('/getHtml', function (req, res) {
 });
 
 app.get('/:id', function (req, res) {
+
     // 首先我们读取已存在的用户
     fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
         data = JSON.parse(data);
         var user = data["user" + req.params.id]
+
         console.log(user);
         res.end(JSON.stringify(user));
     });
-})
+});
+
+function init() {
+    (async () => {
+        for (var i = 0; i < MAX_WSE; i++) {
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: [
+                    '--disable-gpu',
+                    '--disable-dev-shm-usage',
+                    '--disable-setuid-sandbox',
+                    '--no-first-run',
+                    '--no-sandbox',
+                    '--no-zygote',
+                    // '--single-process'
+                ],
+                executablePath: 'C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'
+            });
+            browserWSEndpoint = await browser.wsEndpoint();
+            WSE_LIST[i] = browserWSEndpoint;
+        }
+        console.log(WSE_LIST);
+    })();
+}
 
 app.listen(5000, function () {
-})
+});
