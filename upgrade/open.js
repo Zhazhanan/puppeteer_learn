24cacfa97bc82b56bc86b8fce5958e9259f1d616
wangkun <kunwang1@jieyuechina.com>
2019-05-05 16:25:10 +0800

    init

--- upgrade/open.js
+++
@@ -1,8 +0,0 @@
-var arguments = process.argv.splice(2)[0];
-console.log('所传递的参数是：', arguments);
-
-
-
-const pathToExtension = require('path').join(__dirname, 'my-extension');
-
-console.log('info', pathToExtension);
